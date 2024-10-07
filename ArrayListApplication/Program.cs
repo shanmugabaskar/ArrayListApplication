f36eca5a7d7ec18b244dea457a22e4893e6c2415
shanmugabaskar <baskar.shan@gmail.com>
2024-10-07 10:25:29 +0530

    Add project files.

---
+++ ArrayListApplication/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+
+ArrayList al = new ArrayList();
+
+Console.WriteLine("Adding some numbers:");
+al.Add(45);
+al.Add(78);
+al.Add(33);
+al.Add(56);
+al.Add(12);
+al.Add(23);
+al.Add(9);
+
+Console.WriteLine("Capacity: {0} ", al.Capacity);
+Console.WriteLine("Count: {0}", al.Count);
+
+Console.Write("Content: ");
+foreach (int i in al)
+{
+    Console.Write(i + " ");
+}
+
+Console.WriteLine();
+Console.Write("Sorted Content: ");
+al.Sort();
+foreach (int i in al)
+{
+    Console.Write(i + " ");
+}
+Console.WriteLine();
+Console.ReadKey();
